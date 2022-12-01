cfe8616af5ea73efcb6f54c6f5f6f40370297fb0
Yong-min <rkdrnwkd1@gmail.com>
2022-12-01 17:15:26 +0900

    [feat]백엔드기본세팅

---
+++ Backend/8371be/src/test/java/com/gdsc/be8371/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.be8371;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
