0eca4d934e9f8a95b53da8a84558d34b149b4651
KimHyeonJin <hyeonjin9819@gmail.com>
2022-12-01 19:00:10 +0900

    [feat] event Entity 추가

--- Backend/8371be/src/main/java/com/gdsc/be8371/event/entity/Event.java
+++ Backend/8371be/src/main/java/com/gdsc/be8371/event/entity/Event.java
@@ -1,4 +1,35 @@
 package com.gdsc.be8371.event.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Builder
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private float latitude;
+
+    @Column(nullable = false)
+    private float longitude;
+
+    @Column(nullable = false)
+    private int check;
+
 }
