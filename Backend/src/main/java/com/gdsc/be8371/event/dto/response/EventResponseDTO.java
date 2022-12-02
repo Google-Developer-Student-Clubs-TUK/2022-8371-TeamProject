478f87b60d946f2500d765a2d13ced7cdb5a1891
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-12-03 03:05:59 +0900

    create get_event_all API

--- Backend/src/main/java/com/gdsc/be8371/event/dto/response/EventResponseDTO.java
+++ Backend/src/main/java/com/gdsc/be8371/event/dto/response/EventResponseDTO.java
@@ -1,4 +1,25 @@
 package com.gdsc.be8371.event.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import java.sql.Date;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString
 public class EventResponseDTO {
+    private Integer id;
+    private String title;
+    private String content;
+    private String category;
+    private float latitude;
+    private float longitude;
+    private int checkNum;
+    private Date createdAt;
+    private Date deadLine;
 }
--- Backend/src/main/java/com/gdsc/be8371/event/service/EventServiceImpl.java
+++ Backend/src/main/java/com/gdsc/be8371/event/service/EventServiceImpl.java
@@ -1,8 +1,20 @@
 package com.gdsc.be8371.event.service;
 
+import com.gdsc.be8371.event.entity.Event;
+import com.gdsc.be8371.event.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventServiceImpl implements EventService {
 
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public List<Event> get_event_all() throws Exception {
+        return eventRepository.findAll();
+    }
 }
