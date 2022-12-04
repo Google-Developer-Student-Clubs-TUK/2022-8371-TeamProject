23c644d9c8542b3cadd3a07203181428244f9bb3
jun <ggprgrkjh@naver.com>
2022-12-04 19:44:14 +0900

    feat: Header button 애니메이션 추가

---
+++ Frontend/src/pages/mainpage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import styled from "styled-components";
+//import Animation from "../components/SlideAnimation";
+
+function MainPage() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default MainPage;
